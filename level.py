6e0547a0b85a7d8f1c4ec9c03879e48755911138
abineshthangasamy <abineshrenga@gmail.com>
2026-03-09 18:55:34 -0400

    Added support.py with the import_csv_layout function.

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
 
 class Level:
     def __init__(self):
@@ -18,14 +19,21 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index,row in enumerate(WORLD_MAP):
-            for col_index,col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites)
+        layout = {
+            'boundary': import_csv_layout('map/map_FloorBlocks.csv')
+        }
+
+
+
+        # for row_index,row in enumerate(WORLD_MAP):
+        #     for col_index,col in enumerate(row):
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
+        #         if col == 'x':
+        #             Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
+        #         if col == 'p':
+        #             self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
 
     def run(self):
@@ -41,7 +49,7 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2()
 
         # creating the floor
-        self.floor_surf = pygame.image.load('/Users/abineshthangasamy/PycharmProjects/RPG/graphics/tilemap/ground.png').convert()
+        self.floor_surf = pygame.image.load('Graphics/tilemap/ground.png').convert()
         self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
 
     def custom_draw(self, player):

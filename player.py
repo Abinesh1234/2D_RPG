8c3adedef5db81592e075161ba7f4fd1b9be2f62
abineshthangasamy <abineshrenga@gmail.com>
2026-03-10 19:33:25 -0400

    Added idle checker.

--- player.py
+++ player.py
@@ -12,6 +12,7 @@ class Player(pygame.sprite.Sprite):
 
         # graphics setup
         self.import_player_assets()
+        self.status = 'down'
 
         # movement
         self.direction = pygame.math.Vector2()
@@ -29,7 +30,6 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
-        print(self.animations)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -37,15 +37,19 @@ class Player(pygame.sprite.Sprite):
         # movement input
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
 
@@ -60,6 +64,12 @@ class Player(pygame.sprite.Sprite):
             self.attacking = True
             self.attack_time = pygame.time.get_ticks()
             print('magic')
+
+    def get_status(self):
+        # idle status
+        if self.direction.x == 0 and self.direction.y == 0:
+            if not 'idle' in self.status: # checks if idle isn't already in it
+                self.status = self.status + '_idle'
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
@@ -97,4 +107,5 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.input()
         self.cooldowns()
+        self.get_status()
         self.move(self.speed)

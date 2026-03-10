cd70ed3235e6d2f7e0fcffbde9ea8ca541493be1
abineshthangasamy <abineshrenga@gmail.com>
2026-03-10 19:39:35 -0400

    Logic behind animation works, need to implement animation now.

--- player.py
+++ player.py
@@ -68,8 +68,20 @@ class Player(pygame.sprite.Sprite):
     def get_status(self):
         # idle status
         if self.direction.x == 0 and self.direction.y == 0:
-            if not 'idle' in self.status: # checks if idle isn't already in it
+            if not 'idle' in self.status and not 'attack' in self.status: # checks if idle & attack isn't already in it
                 self.status = self.status + '_idle'
+
+        if self.attacking:
+            self.direction.x = 0
+            self.direction.y = 0
+            if not 'attack' in self.status: # checks if attack isn't already in it
+                if 'idle' in self.status:
+                    self.status = self.status.replace('_idle', '_attack')
+                else:
+                    self.status = self.status + '_attack'
+        else:
+            if 'attack' in self.status:
+                self.status = self.status.replace('_attack', '')
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
